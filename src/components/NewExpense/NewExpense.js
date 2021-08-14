c6b785468ce786f1c5c4dae771bb3d9259f312aa
Alieltahan <alyaltahan@gmail.com>
2021-08-14 11:07:14 +0200

    Updating Filter Year

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -9,7 +9,7 @@ const NewExpense = (props) => {
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
-      id: Math.random().toString(),
+      id: new Date().getTime(),
     };
     props.onAddExpense(expenseData);
     setIsEditing(false);
